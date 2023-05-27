1a32695500816a67aad6c2f578f47df06e5ba136
Miko Dierks <dierks-miko@hotmail.de>
2023-05-27 19:25:40 +0200

    Improve description of SizeChecker

--- module/inventory/SizeChecker.lua
+++ module/inventory/SizeChecker.lua
@@ -2,8 +2,8 @@
 local SizeChecker = {}
 
 --- Checks whether or not an item may fit in this inventory.
---- This is only an initial check to circumvent trying to push an item, requiring one tick of time
---- and may return false positives.
+--- This is only an initial check to circumvent trying to push an item, which requires one tick of time.
+--- This function may return false positives (i.e. return true even if the item does not actually fit)
 --- @param item Item
 --- @return boolean
 function SizeChecker:hasSpaceForItem(item) end
